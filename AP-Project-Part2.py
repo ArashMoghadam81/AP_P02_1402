b42bf82ccb2b38ac2a1980c146c0815bec4f084c
MahdiVelayatipour <130631635+MahdiVelayatipour@users.noreply.github.com>
2023-06-08 23:33:36 +0330

    importing required modules and main body of python file

---
+++ AP-Project-Part2.py
@@ -0,0 +1,45 @@
+#importing required modules and libraris
+from flask import Flask, render_template, request, redirect, session, flash,url_for
+import os
+from werkzeug.utils import secure_filename
+import requests
+
+# using api to exchange USD to IRR
+api_key = "freeLLAbxptI2n0R0VwHjK7fc4laNBpT"
+url = f"http://api.navasan.tech/latest/?api_key={api_key}"
+
+response = requests.get(url)
+if response.status_code == 200:
+    data = response.json()
+
+else:
+    print("API request failed:", response.status_code)
+
+for d in data:
+    if d == "usd_sell":
+        curruncy_index=int(data[d]['value']+'0')
+
+# print(curruncy_index)
+
+app = Flask(__name__)
+app.secret_key = "your_secret_key"
+upload_folder = os.path.join(app.root_path, 'static', 'uploads')
+app.config['UPLOAD_FOLDER'] = upload_folder
+products = []
+users = {}
+cart = []
+
+
+@app.route('/')
+def home():
+    return render_template('Base.html')
+
+@app.route("/logout")
+def logout():
+
+    flash("You have been logged out!")
+    return redirect('/login')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
